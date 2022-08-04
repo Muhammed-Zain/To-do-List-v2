d9cfc8e1af53dee30e347855c0231593ab39105b
Muhammed Zain <mohdzain2002@gmail.com>
2022-08-04 20:13:23 +0530

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -123,6 +123,11 @@ app.post("/delete", (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, () => {
   console.log("This is running successfully on port 3000");
 });
